bed6b99d0f7c1887925433afaaa0af9ae87b0c63
Metse <metse@bnry.digital>
2021-03-05 06:48:48 +0200

    Added cors and koa helmet

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
 import Koa from 'koa';
+import koaBody from 'koa-bodyparser';
+import cors from '@koa/cors';
+import helmet from 'koa-helmet';
 import config from './config';
 import server from './api';
 
 const app = new Koa();
+
+const options = {
+  allowHeaders: 'origin, content-type, accept, authorization',
+  allowMethods: 'POST',
+};
+
+app.use(koaBody());
+app.use(cors(options));
+app.use(helmet());
 
 server.applyMiddleware({ app });
 
